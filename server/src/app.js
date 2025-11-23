df61116a8d14afd9dc1aa48e0eea7741a6d238ff
Andrew Lara <aandrewlaraa@gmail.com>
2025-11-22 19:17:14 -0800

    running into frontend issues connecting to the backend API. debugging now before submitting PR

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,10 @@ import challengesRouter from './routes/challenges.js';
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(pinoHttp({ autoLogging: false }));
